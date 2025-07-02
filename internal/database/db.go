ab07f122c6d3d323e669779e1c8202ef6e8899e3
Aleksandrov Dmitrii <Aleksandrov.D.I@sbertech.ru>
2025-07-03 00:14:36 +0300

    Fix: change driver name from sqlite3 to sqlite

--- internal/database/db.go
+++ internal/database/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func InitDB(dbPath string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := sql.Open("sqlite", dbPath)
 	if err != nil {
 		return nil, fmt.Errorf("ошибка подключения к БД: %w", err)
 	}
